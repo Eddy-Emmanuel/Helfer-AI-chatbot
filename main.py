175b20df116d85a4a478af8f59e4186cd532a2fc
Eddy <edifonemmanuel14@gmail.com>
2025-06-19 09:25:36 +0100

    updated main.py file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from routes import multi_agent
 
@@ -5,3 +6,5 @@ app = FastAPI(root_path="/ai")
 
 app.include_router(router=multi_agent.router)
 
+if __name__ == "__main__":
+    uvicorn.run(app=app, port=8000, host="127.0.0.1")
